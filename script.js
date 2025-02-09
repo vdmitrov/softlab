f3e31e8ad1652da494e1f1453504f3d496a6f5e6
softlab <vdmitrov2001@gmail.com>
2025-02-10 00:02:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,74 +1,14 @@
 const programs = [
-    { 
-        name: "Google Chrome", 
-        category: "Windows", 
-        img: "img/chrome.png", 
-        link: "#", 
-        description: "Быстрый браузер с поддержкой всех современных стандартов." 
-    },
-    { 
-        name: "VLC Media Player", 
-        category: "Windows", 
-        img: "img/vlc.png", 
-        link: "#", 
-        description: "Медиаплеер с поддержкой всех форматов видео и аудио." 
-    },
-    { 
-        name: "WinRAR", 
-        category: "Windows", 
-        img: "img/winrar.png", 
-        link: "#", 
-        description: "Архиватор файлов с поддержкой множества форматов." 
-    },
-    { 
-        name: "Telegram", 
-        category: "Android", 
-        img: "img/Telegram.png", 
-        link: "#", 
-        description: "Безопасный мессенджер с поддержкой чатов и звонков." 
-    },
-    { 
-        name: "WhatsApp", 
-        category: "Android", 
-        img: "img/whatsapp.png", 
-        link: "#", 
-        description: "Популярный мессенджер для обмена сообщениями и звонков." 
-    },
-    { 
-        name: "Notepad++", 
-        category: "Windows", 
-        img: "img/notepad++.png", 
-        link: "#", 
-        description: "Мощный текстовый редактор для программистов." 
-    },
-    { 
-        name: "Firefox", 
-        category: "Windows", 
-        img: "img/firefox.png", 
-        link: "#", 
-        description: "Открытый браузер с высоким уровнем безопасности." 
-    },
-    { 
-        name: "Spotify", 
-        category: "Windows", 
-        img: "img/spotify.png", 
-        link: "#", 
-        description: "Сервис для стриминга музыки с огромной библиотекой." 
-    },
-    { 
-        name: "Instagram", 
-        category: "Android", 
-        img: "img/instagram.png", 
-        link: "#", 
-        description: "Популярная социальная сеть для обмена фотографиями и видео." 
-    },
-    { 
-        name: "Skype", 
-        category: "Windows", 
-        img: "img/skype.png", 
-        link: "#", 
-        description: "Программа для видеозвонков и обмена сообщениями." 
-    }
+    { name: "Google Chrome", category: "Windows", img: "img/chrome.png", link: "#", description: "Быстрый браузер с поддержкой всех современных стандартов." },
+    { name: "VLC Media Player", category: "Windows", img: "img/vlc.png", link: "#", description: "Медиаплеер с поддержкой всех форматов видео и аудио." },
+    { name: "WinRAR", category: "Windows", img: "img/winrar.png", link: "#", description: "Архиватор файлов с поддержкой множества форматов." },
+    { name: "Telegram", category: "Android", img: "img/Telegram.png", link: "#", description: "Безопасный мессенджер с поддержкой чатов и звонков." },
+    { name: "WhatsApp", category: "Android", img: "img/whatsapp.png", link: "#", description: "Популярный мессенджер для обмена сообщениями и звонков." },
+    { name: "Notepad++", category: "Windows", img: "img/notepad++.png", link: "#", description: "Мощный текстовый редактор для программистов." },
+    { name: "Firefox", category: "Windows", img: "img/firefox.png", link: "#", description: "Открытый браузер с высоким уровнем безопасности." },
+    { name: "Spotify", category: "Windows", img: "img/spotify.png", link: "#", description: "Сервис для стриминга музыки с огромной библиотекой." },
+    { name: "Instagram", category: "Android", img: "img/instagram.png", link: "#", description: "Популярная социальная сеть для обмена фотографиями и видео." },
+    { name: "Skype", category: "Windows", img: "img/skype.png", link: "#", description: "Программа для видеозвонков и обмена сообщениями." }
 ];
 
 // Функция отображения программ по категориям
@@ -93,23 +33,4 @@ function displayProgramsByCategory(categories) {
             <p><strong>Описание:</strong> ${program.description}</p>
             <a href="${program.link}" target="_blank"><button>Скачать</button></a>
         `;
-        programList.appendChild(programCard);
-    });
-}
-
-// Вызов функции, чтобы загрузить программы
-window.onload = function() {
-    const currentPage = window.location.pathname;
-
-    // Проверяем, на какой странице мы находимся и отображаем соответствующие программы
-    if (currentPage.includes("android.html")) {
-        displayProgramsByCategory(["Android"]);
-    } else if (currentPage.includes("linux.html")) {
-        displayProgramsByCategory(["Linux"]);
-    } else if (currentPage.includes("windows.html")) {
-        displayProgramsByCategory(["Windows"]);
-    } else {
-        // На главной странице отображаем программы всех категорий
-        displayProgramsByCategory(["Windows", "Android", "Linux"]);
-    }
-};
+        programList
