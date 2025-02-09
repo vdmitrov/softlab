929797fea294571fcf3a883069ae6ff1bc9c7977
softlab <vdmitrov2001@gmail.com>
2025-02-09 21:56:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,54 @@
-// Пример данных для программ
+// Массив с данными о программах
 const programs = [
-    { name: "Программа 1", description: "Описание программы 1", image: "img/program1.png" },
-    { name: "Программа 2", description: "Описание программы 2", image: "img/program2.png" },
-    { name: "Программа 3", description: "Описание программы 3", image: "img/program3.png" },
-    { name: "Программа 4", description: "Описание программы 4", image: "img/program4.png" },
-    { name: "Программа 5", description: "Описание программы 5", image: "img/program5.png" }
-];
-
-// Функция для загрузки программ
-function loadPrograms() {
-    const programList = document.getElementById("program-list");
-
-    programs.forEach(program => {
-        const programCard = document.createElement("div");
-        programCard.classList.add("program");
-
-        programCard.innerHTML = `
-            <img src="${program.image}" alt="${program.name}">
-            <h3>${program.name}</h3>
-            <p>${program.description}</p>
-            <button>Установить</button>
-        `;
-
-        programList.appendChild(programCard);
-    });
-}
-
-// Вызовем функцию, чтобы загрузить программы при загрузке страницы
-window.onload = loadPrograms;
+    { 
+        name: "Google Chrome", 
+        category: "Windows", 
+        img: "img/chrome.png", 
+        link: "#", 
+        description: "Быстрый браузер с поддержкой всех современных стандартов." 
+    },
+    { 
+        name: "VLC Media Player", 
+        category: "Windows", 
+        img: "img/vlc.png", 
+        link: "#", 
+        description: "Медиаплеер с поддержкой всех форматов видео и аудио." 
+    },
+    { 
+        name: "WinRAR", 
+        category: "Windows", 
+        img: "img/winrar.png", 
+        link: "#", 
+        description: "Архиватор файлов с поддержкой множества форматов." 
+    },
+    { 
+        name: "Telegram", 
+        category: "Android", 
+        img: "img/telegram.png", 
+        link: "#", 
+        description: "Безопасный мессенджер с поддержкой чатов и звонков." 
+    },
+    { 
+        name: "WhatsApp", 
+        category: "Android", 
+        img: "img/whatsapp.png", 
+        link: "#", 
+        description: "Популярный мессенджер для обмена сообщениями и звонков." 
+    },
+    { 
+        name: "Notepad++", 
+        category: "Windows", 
+        img: "img/notepad++.png", 
+        link: "#", 
+        description: "Мощный текстовый редактор для программистов." 
+    },
+    { 
+        name: "Firefox", 
+        category: "Windows", 
+        img: "img/firefox.png", 
+        link: "#", 
+        description: "Открытый браузер с высоким уровнем безопасности." 
+    },
+    { 
+        name: "Spotify", 
+        category: "Windo
