9c1e6f0b5370681e9c260fa1afe00f4005aa153d
softlab <vdmitrov2001@gmail.com>
2025-02-09 21:53:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,30 @@
+// Пример данных для программ
 const programs = [
-    { 
-        name: "Google Chrome", 
-        category: "Windows", 
-        img: "img/chrome.png", 
-        link: "#", 
-        description: "Быстрый и удобный веб-браузер от Google. "
-    },
-    { 
-        name: "VLC Media Player", 
-        category: "Windows", 
-        img: "img/vlc.png", 
-        link: "#", 
-        description: "Популярный медиаплеер с поддержкой множества форматов."
-    },
-    { 
-        name: "WinRAR", 
-        category: "Windows", 
-        img: "img/Winrar.png", 
-        link: "#", 
-        description: "Архиватор файлов с высокой степенью сжатия. "
-    },
-    { 
-        name: "Telegram", 
-        category: "Android", 
-        img: "img/Telegram.png", 
-        link: "#", 
-        description: "Быстрый и безопасный мессенджер. "
-    },
-    { 
-        name: "WhatsApp", 
-        category: "Android", 
-        img: "img/whatsapp.png", 
-        link: "#", 
-        description: "Один из самых популярных мессенджеров в мире. "
-    }
-
-
-
+    { name: "Программа 1", description: "Описание программы 1", image: "img/program1.png" },
+    { name: "Программа 2", description: "Описание программы 2", image: "img/program2.png" },
+    { name: "Программа 3", description: "Описание программы 3", image: "img/program3.png" },
+    { name: "Программа 4", description: "Описание программы 4", image: "img/program4.png" },
+    { name: "Программа 5", description: "Описание программы 5", image: "img/program5.png" }
 ];
 
-function displayPrograms(programList) {
-    const listContainer = document.getElementById("program-list");
-    listContainer.innerHTML = ""; 
+// Функция для загрузки программ
+function loadPrograms() {
+    const programList = document.getElementById("program-list");
 
-    programList.forEach(program => {
-        const div = document.createElement("div");
-        div.classList.add("program");
-        div.innerHTML = `
-            <img src="${program.img}" alt="${program.name}">
+    programs.forEach(program => {
+        const programCard = document.createElement("div");
+        programCard.classList.add("program");
+
+        programCard.innerHTML = `
+            <img src="${program.image}" alt="${program.name}">
             <h3>${program.name}</h3>
-            <p><strong>${program.category}</strong></p>
-            <p class="description">${program.description}</p>
-            <button onclick="location.href='${program.link}'">Скачать</button>
+            <p>${program.description}</p>
+            <button>Установить</button>
         `;
-        listContainer.appendChild(div);
+
+        programList.appendChild(programCard);
     });
 }
 
-function searchPrograms() {
-    const query = document.getElementById("search").value.toLowerCase();
-    const filteredPrograms = programs.filter(program => 
-        program.name.toLowerCase().includes(query)
-    );
-    displayPrograms(filteredPrograms);
-}
-
-window.onload = () => {
-    displayPrograms(programs);
-};
+// Вызовем функцию, чтобы загрузить программы при загрузке страницы
+window.onload = loadPrograms;
